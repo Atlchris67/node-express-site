7dec8c31bfee9dbd047fec773d7dab4240abea78
Atlchris67 <atlchris@gmail.com>
2020-10-11 20:34:23 -0400

    stopped port doubling"

--- app.js
+++ app.js
@@ -1,15 +1,8 @@
 //Imports the express module
 const express = require('express');
 
-const path = require('path')
-const PORT = process.env.PORT || 5000
+const path = require('path');
 
-express()
-  .use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
 
 //A top-level function exported by the express module
 //Adapted from: https://freshman.tech/learn-node/
